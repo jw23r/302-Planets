3ed95a22ce31234b32ea3f423e54b5feef6dbe45
jesse <webbj19@ferris.edu>
2020-05-12 17:17:34 -0400

    changed orbit to no longer be co planer if it is not a planet

--- Assets/scripts/Orbit.cs
+++ Assets/scripts/Orbit.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class Orbit : MonoBehaviour
 {
+    public float unCoplaner;
     public Transform orbitCenter;
     public LineRenderer orbitPath;
-    [Range(10000, 970000)] public float radius = 6;
+    [Range(0, 1000)] public float radius = 6;
     [Range(4, 48)] public int resolution = 8;
     public float planetSpeed = 1;
+    public bool planet = true;
     
     // Start is called before the first frame update
     void Start()
@@ -22,16 +24,24 @@ public class Orbit : MonoBehaviour
     void Update()
     {
 
-        Vector3 pos = FindOrbitPoint(planetSpeed* UinverseTime.worldTime, radius);
+        Vector3 pos = FindOrbitPoint(planetSpeed* UinverseTime.worldTime, radius, unCoplaner);
         transform.position = pos;
         UpdatePoints();
     }
-    private Vector3 FindOrbitPoint(float angle, float mag)
+    private Vector3 FindOrbitPoint(float angle, float mag, float magUnCoplaner)
     {
        
         Vector3 pos = (orbitCenter == null) ? Vector3.zero : orbitCenter.position;
-        pos.x += Mathf.Cos(angle) * mag;
-        pos.z += Mathf.Sin(angle) * mag;
+        if (planet)
+        {
+            pos.x += Mathf.Cos(angle) * mag;
+            pos.z += Mathf.Sin(angle) * mag;
+        }
+        else
+        {
+            pos.x += Mathf.Cos(angle) * magUnCoplaner;
+            pos.z += Mathf.Sin(angle) * mag;
+        }
         return pos;
     }
     void UpdatePoints()
@@ -43,7 +53,7 @@ public class Orbit : MonoBehaviour
             //calculate x,y,andz
             float p = i / (float)points.Length;
             float radians = p * Mathf.PI * 2;
-            points[i] = FindOrbitPoint(radians, radius);
+            points[i] = FindOrbitPoint(radians, radius,unCoplaner);
         }
         orbitPath.positionCount = resolution;
         orbitPath.SetPositions(points);
